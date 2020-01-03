f77d2718c46be6438e92ccb80555f1449a2ef87c
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2020-01-03 09:58:43 -0300

    add runas appspec, fix .env

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -1,12 +1,15 @@
-cd /var/www/html
+cd /home/ubuntu/app
 
 composer install
 
 touch .env
 
-echo DB_DATABASE=$DATABASE_URL >> .env
-echo DB_USERNAME=$DATABASE_USERNAME >> .env
-echo DB_PASSWORD=$DATABASE_PASSWORD >> .env
+echo DB_HOST=$DATABASE_URL >>.env
+echo DB_USERNAME=$DATABASE_USERNAME >>.env
+echo DB_PASSWORD=$DATABASE_PASSWORD >>.env
+echo DB_DATABASE=$DATABASE_NAME >>.env
+echo DB_PORT=3306 >>.env
+echo DB_CONNECTION=mysql >>.env
 
 composer dump-autoload
 
