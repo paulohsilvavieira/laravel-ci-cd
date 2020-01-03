f3b9ab19d6b48dee0fd0da640c26e3b5525c7419
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2020-01-03 11:52:06 -0300

    Fix permissions files again

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -1,64 +1,65 @@
 rm /home/ubuntu/.env
+cd /home/ubuntu
 
-if [ ! -f "/home/ubuntu/.env" ]; then
+if [ ! -f .env ]; then
 
-    echo 'APP_NAME=Laravel' >> /home/ubuntu/.env
-    echo 'APP_ENV=local' >> /home/ubuntu/.env
-    echo 'APP_KEY=' >> /home/ubuntu/.env
-    echo 'APP_DEBUG=false' >> /home/ubuntu/.env
-    echo 'APP_URL=http://localhost' >> /home/ubuntu/.env
-    echo ""  >> /home/ubuntu/.env
-    echo 'LOG_CHANNEL=stack' >> /home/ubuntu/.env
+    echo 'APP_NAME=Laravel' >> .env
+    echo 'APP_ENV=local' >> .env
+    echo 'APP_KEY=' >> .env
+    echo 'APP_DEBUG=false' >> .env
+    echo 'APP_URL=http://localhost' >> .env
+    echo ""  >> .env
+    echo 'LOG_CHANNEL=stack' >> .env
 
-    echo ""  >> /home/ubuntu/.env
+    echo ""  >> .env
 
-    echo 'BROADCAST_DRIVER=log' >> /home/ubuntu/.env
-    echo 'CACHE_DRIVER=file' >> /home/ubuntu/.env
-    echo 'QUEUE_CONNECTION=sync' >> /home/ubuntu/.env
-    echo 'SESSION_DRIVER=cookie' >> /home/ubuntu/.env
-    echo 'SESSION_LIFETIME=120' >> /home/ubuntu/.env
-    echo ""  >> /home/ubuntu/.env
+    echo 'BROADCAST_DRIVER=log' >> .env
+    echo 'CACHE_DRIVER=file' >> .env
+    echo 'QUEUE_CONNECTION=sync' >> .env
+    echo 'SESSION_DRIVER=cookie' >> .env
+    echo 'SESSION_LIFETIME=120' >> .env
+    echo ""  >> .env
 
-    echo 'REDIS_HOST=127.0.0.1' >> /home/ubuntu/.env
-    echo 'REDIS_PASSWORD=null' >> /home/ubuntu/.env
-    echo 'REDIS_PORT=6379' >> /home/ubuntu/.env
-    echo ""  >> /home/ubuntu/.env
+    echo 'REDIS_HOST=127.0.0.1' >> .env
+    echo 'REDIS_PASSWORD=null' >> .env
+    echo 'REDIS_PORT=6379' >> .env
+    echo ""  >> .env
 
-    echo 'MAIL_DRIVER=smtp' >> /home/ubuntu/.env
-    echo 'MAIL_HOST=smtp.mailtrap.io' >> /home/ubuntu/.env
-    echo 'MAIL_PORT=2525' >> /home/ubuntu/.env
-    echo 'MAIL_USERNAME=null' >> /home/ubuntu/.env
-    echo 'MAIL_PASSWORD=null' >> /home/ubuntu/.env
-    echo 'MAIL_ENCRYPTION=null' >> /home/ubuntu/.env
-    echo 'MAIL_FROM_ADDRESS=null' >> /home/ubuntu/.env
-    echo 'MAIL_FROM_NAME="${APP_NAME}"' >> /home/ubuntu/.env
-    echo ""  >> /home/ubuntu/.env
+    echo 'MAIL_DRIVER=smtp' >> .env
+    echo 'MAIL_HOST=smtp.mailtrap.io' >> .env
+    echo 'MAIL_PORT=2525' >> .env
+    echo 'MAIL_USERNAME=null' >> .env
+    echo 'MAIL_PASSWORD=null' >> .env
+    echo 'MAIL_ENCRYPTION=null' >> .env
+    echo 'MAIL_FROM_ADDRESS=null' >> .env
+    echo 'MAIL_FROM_NAME="${APP_NAME}"' >> .env
+    echo ""  >> .env
 
-    echo 'AWS_ACCESS_KEY_ID=' >> /home/ubuntu/.env
-    echo 'AWS_SECRET_ACCESS_KEY=' >> /home/ubuntu/.env
-    echo 'AWS_DEFAULT_REGION=us-east-1' >> /home/ubuntu/.env
-    echo 'AWS_BUCKET=' >> /home/ubuntu/.env
-    echo ""  >> /home/ubuntu/.env
+    echo 'AWS_ACCESS_KEY_ID=' >> .env
+    echo 'AWS_SECRET_ACCESS_KEY=' >> .env
+    echo 'AWS_DEFAULT_REGION=us-east-1' >> .env
+    echo 'AWS_BUCKET=' >> .env
+    echo ""  >> .env
 
-    echo 'PUSHER_APP_ID=' >> /home/ubuntu/.env
-    echo 'PUSHER_APP_KEY=' >> /home/ubuntu/.env
-    echo 'PUSHER_APP_SECRET=' >> /home/ubuntu/.env
-    echo 'PUSHER_APP_CLUSTER=mt1' >> /home/ubuntu/.env
-    echo ""  >>/home/ubuntu/.env
+    echo 'PUSHER_APP_ID=' >> .env
+    echo 'PUSHER_APP_KEY=' >> .env
+    echo 'PUSHER_APP_SECRET=' >> .env
+    echo 'PUSHER_APP_CLUSTER=mt1' >> .env
+    echo ""  >>.env
 
-    echo 'MIX_PUSHER_APP_KEY="${PUSHER_APP_KEY}"' >> /home/ubuntu/.env
-    echo 'MIX_PUSHER_APP_CLUSTER="${PUSHER_APP_CLUSTER}"' >> /home/ubuntu/.env
+    echo 'MIX_PUSHER_APP_KEY="${PUSHER_APP_KEY}"' >> .env
+    echo 'MIX_PUSHER_APP_CLUSTER="${PUSHER_APP_CLUSTER}"' >> .env
 
-    echo ""  >> /home/ubuntu/.env
+    echo ""  >> .env
 
-    echo 'DB_CONNECTION=mysql' >> /home/ubuntu/.env
-    echo "DB_HOST=$DATABASE_URL" >> /home/ubuntu/.env
-    echo 'DB_PORT=3306' >> /home/ubuntu/.env
-    echo "DB_DATABASE=$DATABASE_NAME" >> /home/ubuntu/.env
-    echo "DB_USERNAME=$DATABASE_USERNAME" >> /home/ubuntu/.env
-    echo "DB_PASSWORD=$DATABASE_PASSWORD" >> /home/ubuntu/.env
+    echo 'DB_CONNECTION=mysql' >> .env
+    echo "DB_HOST=$DATABASE_URL" >> .env
+    echo 'DB_PORT=3306' >> .env
+    echo "DB_DATABASE=$DATABASE_NAME" >> .env
+    echo "DB_USERNAME=$DATABASE_USERNAME" >> .env
+    echo "DB_PASSWORD=$DATABASE_PASSWORD" >> .env
 
-    cp /home/ubuntu/.env /home/ubuntu/app/
+    cp .env /home/ubuntu/app/
 fi
 
 sudo chmod -R 777 /home/ubuntu/app
