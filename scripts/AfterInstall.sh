06b265e8ab30b95f0c7fe460b1661b7799a6daf4
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2020-01-03 10:51:03 -0300

    Fix Bugs

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -7,53 +7,60 @@ composer install
 sudo chgrp -R ubuntu storage bootstrap/cache
 sudo chmod -R ubuntu storage bootstrap/cache
 
+sudo echo 'APP_NAME=Laravel' >>.env
+sudo echo 'APP_ENV=local' >>.env
+sudo echo 'APP_KEY=' >>.env
+sudo echo 'APP_DEBUG=false' >>.env
+sudo echo 'APP_URL=http://localhost' >>.env
+sudo echo ""  >>.env
+sudo echo 'LOG_CHANNEL=stack' >>.env
 
-echo "DB_HOST=$DATABASE_URL" >>.env
-echo "DB_USERNAME=$DATABASE_USERNAME" >>.env
-echo "DB_PASSWORD=$DATABASE_PASSWORD" >>.env
-echo "DB_DATABASE=$DATABASE_NAME" >>.env
+sudo echo 'DB_CONNECTION=mysql' >>.env
+sudo echo "DB_HOST=$DATABASE_URL" >>.env
+sudo echo 'DB_PORT=3306' >>.env
+sudo echo "DB_DATABASE=$DATABASE_NAME" >>.env
+sudo echo "DB_USERNAME=$DATABASE_USERNAME" >>.env
+sudo echo "DB_PASSWORD=$DATABASE_PASSWORD" >>.env
 
-echo 'DB_PORT=3306' >>.env
-echo 'DB_CONNECTION=mysql' >>.env
-echo 'APP_NAME=Laravel' >>.env
-echo 'APP_ENV=local' >>.env
+sudo echo ""  >>.env
 
-echo 'APP_DEBUG=false' >>.env
-echo 'APP_URL=http://localhost' >>.env
 
-echo 'LOG_CHANNEL=stack' >>.env
+sudo echo 'BROADCAST_DRIVER=log' >>.env
+sudo echo 'CACHE_DRIVER=file' >>.env
+sudo echo 'QUEUE_CONNECTION=sync' >>.env
+sudo echo 'SESSION_DRIVER=cookie' >>.env
+sudo echo 'SESSION_LIFETIME=120' >>.env
+sudo echo ""  >>.env
 
-echo 'BROADCAST_DRIVER=log' >>.env
-echo 'CACHE_DRIVER=file' >>.env
-echo 'QUEUE_CONNECTION=sync' >>.env
-echo 'SESSION_DRIVER=cookie' >>.env
-echo 'SESSION_LIFETIME=120' >>.env
+sudo echo 'REDIS_HOST=127.0.0.1' >>.env
+sudo echo 'REDIS_PASSWORD=null' >>.env
+sudo echo 'REDIS_PORT=6379' >>.env
+sudo echo ""  >>.env
 
-echo 'REDIS_HOST=127.0.0.1' >>.env
-echo 'REDIS_PASSWORD=null' >>.env
-echo 'REDIS_PORT=6379' >>.env
+sudo echo 'MAIL_DRIVER=smtp' >>.env
+sudo echo 'MAIL_HOST=smtp.mailtrap.io' >>.env
+sudo echo 'MAIL_PORT=2525' >>.env
+sudo echo 'MAIL_USERNAME=null' >>.env
+sudo echo 'MAIL_PASSWORD=null' >>.env
+sudo echo 'MAIL_ENCRYPTION=null' >>.env
+sudo echo 'MAIL_FROM_ADDRESS=null' >>.env
+sudo echo 'MAIL_FROM_NAME="${APP_NAME}"' >>.env
+sudo echo ""  >>.env
 
-echo 'MAIL_DRIVER=smtp' >>.env
-echo 'MAIL_HOST=smtp.mailtrap.io' >>.env
-echo 'MAIL_PORT=2525' >>.env
-echo 'MAIL_USERNAME=null' >>.env
-echo 'MAIL_PASSWORD=null' >>.env
-echo 'MAIL_ENCRYPTION=null' >>.env
-echo 'MAIL_FROM_ADDRESS=null' >>.env
-echo 'MAIL_FROM_NAME="${APP_NAME}"' >>.env
+sudo echo 'AWS_ACCESS_KEY_ID=' >>.env
+sudo echo 'AWS_SECRET_ACCESS_KEY=' >>.env
+sudo echo 'AWS_DEFAULT_REGION=us-east-1' >>.env
+sudo echo 'AWS_BUCKET=' >>.env
+sudo echo ""  >>.env
 
-echo 'AWS_ACCESS_KEY_ID=' >>.env
-echo 'AWS_SECRET_ACCESS_KEY=' >>.env
-echo 'AWS_DEFAULT_REGION=us-east-1' >>.env
-echo 'AWS_BUCKET=' >>.env
+sudo echo 'PUSHER_APP_ID=' >>.env
+sudo echo 'PUSHER_APP_KEY=' >>.env
+sudo echo 'PUSHER_APP_SECRET=' >>.env
+sudo echo 'PUSHER_APP_CLUSTER=mt1' >>.env
+sudo echo ""  >>.env
 
-echo 'PUSHER_APP_ID=' >>.env
-echo 'PUSHER_APP_KEY=' >>.env
-echo 'PUSHER_APP_SECRET=' >>.env
-echo 'PUSHER_APP_CLUSTER=mt1' >>.env
-
-echo 'MIX_PUSHER_APP_KEY="${PUSHER_APP_KEY}"' >>.env
-echo 'MIX_PUSHER_APP_CLUSTER="${PUSHER_APP_CLUSTER}"' >>.env
+sudo echo 'MIX_PUSHER_APP_KEY="${PUSHER_APP_KEY}"' >>.env
+sudo echo 'MIX_PUSHER_APP_CLUSTER="${PUSHER_APP_CLUSTER}"' >>.env
 
 composer dump-autoload
 
