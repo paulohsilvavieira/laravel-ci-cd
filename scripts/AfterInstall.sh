124d38032677f0ebf1512b8a06a8b467548fe5fd
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2020-01-03 09:20:04 -0300

    Fix .env create process

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -2,14 +2,11 @@ cd /var/www/html
 
 composer install
 
-npm install
-
-
 touch .env
 
-echo 'DB_DATABASE=$DATABASE_URL' >> .env
-echo 'DB_USERNAME=$DATABASE_USERNAME' >> .env
-echo 'DB_PASSWORD=$DATABASE_PASSWORD' >> .env
+echo DB_DATABASE=$DATABASE_URL >> .env
+echo DB_USERNAME=$DATABASE_USERNAME >> .env
+echo DB_PASSWORD=$DATABASE_PASSWORD >> .env
 
 php artisan key:generate
 
