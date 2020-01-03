c212993b47a004ff2a91ffb1ac659eedd1ad7729
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2020-01-03 10:06:18 -0300

    fix error

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -1,17 +1,26 @@
 cd /home/ubuntu/app
 
+sudo chmod -R 755 .
+
 composer install
 
 touch .env
 
-echo DB_HOST=$DATABASE_URL >>.env
-echo DB_USERNAME=$DATABASE_USERNAME >>.env
-echo DB_PASSWORD=$DATABASE_PASSWORD >>.env
+echo DB_HOST=$DATABASE_URL >> .env
+
+echo DB_USERNAME=$DATABASE_USERNAME >> .env
+
+echo DB_PASSWORD=$DATABASE_PASSWORD >> .env
+
 echo DB_DATABASE=$DATABASE_NAME >>.env
-echo DB_PORT=3306 >>.env
-echo DB_CONNECTION=mysql >>.env
+
+
+echo 'DB_PORT=3306' >>.env
+
+echo 'DB_CONNECTION=mysql' >>.env
 
 composer dump-autoload
+
 php artisan key:generate
 
 php artisan migrate
