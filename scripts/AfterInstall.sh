9f3e14ea895806491b21bb519d94e13bcdbb617c
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2020-01-03 09:25:02 -0300

    add runas appspec

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -8,6 +8,9 @@ echo DB_DATABASE=$DATABASE_URL >> .env
 echo DB_USERNAME=$DATABASE_USERNAME >> .env
 echo DB_PASSWORD=$DATABASE_PASSWORD >> .env
 
+composer dump-autoload
+
 php artisan key:generate
+
 
 
