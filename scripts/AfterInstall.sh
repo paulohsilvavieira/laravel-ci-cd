5ba0f5a5b75d754f6831d4622246e7f4b91d62dd
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2020-01-03 10:40:24 -0300

    Fix Bugs

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -8,10 +8,10 @@ sudo chgrp -R $USER storage bootstrap/cache
 sudo chmod -R $USER storage bootstrap/cache
 
 
-echo DB_HOST=$DATABASE_URL >>.env
-echo DB_USERNAME=$DATABASE_USERNAME >>.env
-echo DB_PASSWORD=$DATABASE_PASSWORD >>.env
-echo DB_DATABASE=$DATABASE_NAME >>.env
+echo "DB_HOST=$DATABASE_URL" >>.env
+echo "DB_USERNAME=$DATABASE_USERNAME" >>.env
+echo "DB_PASSWORD=$DATABASE_PASSWORD" >>.env
+echo "DB_DATABASE=$DATABASE_NAME" >>.env
 
 echo 'DB_PORT=3306' >>.env
 echo 'DB_CONNECTION=mysql' >>.env
