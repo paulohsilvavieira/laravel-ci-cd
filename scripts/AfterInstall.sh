fd69eae9a0523b29b0da2d87c371214cd3e339b0
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2020-01-03 06:14:13 -0300

    Change After Install add environments

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -1,1 +1,16 @@
-echo $DATABASE_PASSWORD
+cd /var/www/html
+
+composer install
+
+npm install
+
+
+touch .env
+
+echo 'DB_DATABASE=$DATABASE_URL' >> .env
+echo 'DB_USERNAME=$DATABASE_USERNAME' >> .env
+echo 'DB_PASSWORD=$DATABASE_PASSWORD' >> .env
+
+php artisan key:generate
+
+
